5c6cb434b587956d66944125a35a6dabdc4ba9e7
12999493 <12999493@itisrossi.vi.it>
2025-11-24 12:52:23 +0100

    Initial commit

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+int main(void) {
+    int n;
+    printf("Inserisci il numero che rappresenta il numero di processi da creare\n");
+    scanf("%d",&n);
+    for (int i = 1; i<=n; i++) {
+        pid_t ritorno = fork();
+        if (ritorno == -1) {
+            printf("Errore fork\n");
+            exit(-1);
+        }
+        if (ritorno == 0) {
+            printf("Processo figlio numero %d\n",i);
+            printf("PID figlio: %d\n",getpid());
+            printf("PID di mio padre: %d\n",getppid());
+            exit(0);
+        }
+        else {
+            wait(NULL);
+            printf("Processo padre numero: %d\n",i);
+            printf("PID padre: %d\n",getpid());
+            printf("PID di mio figlio: %d\n",ritorno);
+        }
+    }
+    return 0;
+}
